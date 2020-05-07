6ad74e0d22341ef6a636d7a924b9e88a3dfba692
thegauravjawla <gauravjawla18@gmail.com>
2020-05-08 00:54:05 +0530

    Version 5 of YelpCamp

--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
     {
         name: "Clouds resr",
         image: "https://images.unsplash.com/photo-1534187886935-1e1236e856c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-        description: "hello hello lololo XOXOXO"
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non fuga eos delectus tempore. Qui nisi inventore voluptatum porro, vero sint, recusandae optio possimus cumque laboriosam veritatis unde libero odit. Velit ab sunt enim, nesciunt excepturi cum ex quas obcaecati blanditiis inventore iste nisi impedit reprehenderit distinctio eligendi tenetur suscipit ut facere eaque commodi harum iusto. Eum molestias doloribus totam ipsam id. Beatae reprehenderit reiciendis quae repellat sapiente a totam harum, illum perferendis nesciunt mollitia voluptatibus expedita facilis ipsum soluta tenetur aut iusto. Odio nostrum earum praesentium itaque doloribus libero consequatur debitis eligendi ad aut at inventore labore, obcaecati iure officia."
     },
     {
         name: "Desert Skill",
         image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-        description: "hello hello lololo XOXOXO"
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non fuga eos delectus tempore. Qui nisi inventore voluptatum porro, vero sint, recusandae optio possimus cumque laboriosam veritatis unde libero odit. Velit ab sunt enim, nesciunt excepturi cum ex quas obcaecati blanditiis inventore iste nisi impedit reprehenderit distinctio eligendi tenetur suscipit ut facere eaque commodi harum iusto. Eum molestias doloribus totam ipsam id. Beatae reprehenderit reiciendis quae repellat sapiente a totam harum, illum perferendis nesciunt mollitia voluptatibus expedita facilis ipsum soluta tenetur aut iusto. Odio nostrum earum praesentium itaque doloribus libero consequatur debitis eligendi ad aut at inventore labore, obcaecati iure officia."
     },
     {
         name: "Kenyan Glorerr",
         image: "https://images.unsplash.com/photo-1576949223184-431f5c6eb29b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-        description: "hello hello lololo XOXOXO"
+        description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non fuga eos delectus tempore. Qui nisi inventore voluptatum porro, vero sint, recusandae optio possimus cumque laboriosam veritatis unde libero odit. Velit ab sunt enim, nesciunt excepturi cum ex quas obcaecati blanditiis inventore iste nisi impedit reprehenderit distinctio eligendi tenetur suscipit ut facere eaque commodi harum iusto. Eum molestias doloribus totam ipsam id. Beatae reprehenderit reiciendis quae repellat sapiente a totam harum, illum perferendis nesciunt mollitia voluptatibus expedita facilis ipsum soluta tenetur aut iusto. Odio nostrum earum praesentium itaque doloribus libero consequatur debitis eligendi ad aut at inventore labore, obcaecati iure officia."
     }
 ];
 
