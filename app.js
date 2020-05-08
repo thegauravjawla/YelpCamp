f7f5df45248fc1810e55ca74a860453b287d41c0
thegauravjawla <gauravjawla18@gmail.com>
2020-05-08 21:44:00 +0530

    removed local server

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments/", commentRoutes);
 
-app.listen(process.env.PORT || 3000, function(){ 
+app.listen(process.env.PORT, function(){ 
     console.log("YelpCamp server is live");
 });
