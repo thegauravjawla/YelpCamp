2e12ac39594a8e41a7d8346ecef678c501e28cae
thegauravjawla <gauravjawla18@gmail.com>
2020-05-08 21:50:53 +0530

    replace the

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments/", commentRoutes);
 
-app.listen(process.env.PORT, function(){ 
+app.listen(process.env.PORT || 3000, function(){ 
     console.log("YelpCamp server is live");
 });
